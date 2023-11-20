2e7b3143da10166d3d5667198d2831a93aa41721
osgens <osgens@student.42.fr>
2023-11-20 19:52:59 +0000

    dq

--- ex08/main.c
+++ ex08/main.c
@@ -24,7 +24,7 @@ static struct miscdevice myfd_device = {
         .fops = &myfd_fops
 };
 
-char str[PAGE_SIZE];
+static char str[PAGE_SIZE]; //page size * 2 is assigned so siwed increased
 // char *tmp;
 
 static int __init myfd_init(void) {
@@ -40,22 +40,27 @@ static void __exit myfd_cleanup(void) {
 }
 
 ssize_t myfd_read(struct file *fp, char __user *user, size_t size, loff_t *offs) {
-        size_t t, i;
-        char *tmp;
+        int t, i; //fixed as the loop is going negative
+        char *tmp; //fixed: no need for a global
+        int ret;
         tmp = kmalloc(sizeof(char) * PAGE_SIZE * 2, GFP_KERNEL);
         // tmp = tmp2;
         for (t = strlen(str) - 1, i = 0; t >= 0; t--, i++) {
                 tmp[i] = str[t];
         }
         tmp[i] = 0x0;
-        return simple_read_from_buffer(user, size, offs, tmp, i);
+        ret = simple_read_from_buffer(user, size, offs, tmp, i);
+        kfree(tmp);
+        return ret;
 }
 
 ssize_t myfd_write(struct file *fp, const char __user *user, size_t size, loff_t *offs) {
         ssize_t res;
 
-        res = simple_write_to_buffer(str, size, offs, user, size) + 1; // 0x0 = ’\0’
-        str[res] = 0x0;
+        res = simple_write_to_buffer(str, PAGE_SIZE - 1, offs, user, size) + 1; // 0x0 = ’\0’
+        if (res <= 1)
+                return 0;
+        str[res - 1] = 0x0;
         return res;
 }
 
---
+++ ex08/test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cat /dev/reverse
+echo 123 > /dev/reverse
+cat /dev/reverse
+
+echo reverse > /dev/reverse
+cat /dev/reverse
+echo
