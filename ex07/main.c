fbfe2b41be5cba820e1796f8b6249396974be3af
osgens <osgens@student.42.fr>
2023-11-20 16:06:30 +0000

    dq

---
+++ ex07/main.c
@@ -0,0 +1,147 @@
+#include <linux/fs.h>
+#include <linux/debugfs.h>
+#include <linux/jiffies.h>
+#include <linux/module.h>
+#include <linux/uaccess.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Ogenser");
+MODULE_DESCRIPTION("Task 06 from 42 little-penguin project");
+MODULE_VERSION("0.1");
+
+#define STUDENT_LOGIN "ogenser"
+#define STUDENT_LOGIN_LEN 7
+
+static char foo_data[PAGE_SIZE]; // Buffer for "foo" file
+
+static ssize_t id_read(struct file *file, char __user *buf, size_t count, loff_t *pos) {
+//      pr_info("Read function called");
+        // Check if already at the end of the file
+        if (*pos != 0) {
+            return 0;  // End of file
+        }
+
+        // Copy the student login to the user buffer
+        if (copy_to_user(buf, STUDENT_LOGIN, strlen(STUDENT_LOGIN))) {
+            return -EFAULT;  // Bad address
+        }
+
+        // Update the position and return the length of the login
+        *pos += strlen(STUDENT_LOGIN);
+        return strlen(STUDENT_LOGIN);
+}
+
+static ssize_t id_write(struct file *file, const char __user *buf, size_t count, loff_t *pos) {
+        char temp[STUDENT_LOGIN_LEN];
+        pr_info("Write function called");
+        //check input lenght // Copy 9 bytes from user // Check if the written value matches the student login
+	if ((count != STUDENT_LOGIN_LEN+1) ||            
+		(copy_from_user(temp, buf, STUDENT_LOGIN_LEN)) ||
+		(strncmp(temp, STUDENT_LOGIN, STUDENT_LOGIN_LEN)))
+	        return -EINVAL;
+	else
+	  return count;
+}
+
+static ssize_t jiffies_read(struct file *file, char __user *buf, size_t count, loff_t *pos) {
+    char buffer[20]; // Buffer to hold the string representation of jiffies
+    int len;
+
+    if (*pos != 0) {
+        return 0;  // End of file
+    }
+
+    len = snprintf(buffer, sizeof(buffer), "%lu\n", jiffies);
+
+    if (copy_to_user(buf, buffer, len)) {
+        return -EFAULT;  // Bad address
+    }
+
+    *pos += len;
+    return len;
+}
+
+static ssize_t foo_read(struct file *file, char __user *buf, size_t count, loff_t *pos) {
+    ssize_t ret = 0;
+
+    if (*pos >= PAGE_SIZE) {
+        return 0;  // End of file
+    }
+
+    if (*pos + count > PAGE_SIZE) {
+        count = PAGE_SIZE - *pos;
+    }
+
+    if (copy_to_user(buf, foo_data + *pos, count)) {
+        return -EFAULT;  // Bad address
+    }
+
+    *pos += count;
+    ret = count;
+
+    return ret;
+}
+
+static ssize_t foo_write(struct file *file, const char __user *buf, size_t count, loff_t *pos) {
+    ssize_t ret = 0;
+
+    if (*pos >= PAGE_SIZE) {
+        return -ENOSPC;  // No space left on device
+    }
+
+    if (*pos + count > PAGE_SIZE) {
+        count = PAGE_SIZE - *pos;
+    }
+
+    if (copy_from_user(foo_data + *pos, buf, count)) {
+        return -EFAULT;  // Bad address
+    }
+
+    *pos += count;
+    ret = count;
+
+    return ret;
+}
+
+static const struct file_operations id_fops = {
+    .owner = THIS_MODULE,
+    .read = id_read,
+    .write = id_write,
+};
+
+static const struct file_operations jiffies_fops = {
+    .owner = THIS_MODULE,
+    .read = jiffies_read,
+};
+
+static const struct file_operations foo_fops = {
+    .owner = THIS_MODULE,
+    .read = foo_read,
+    .write = foo_write,
+};
+
+static struct dentry *fortytwo_dir;
+
+static int __init ex06_init(void) {
+    fortytwo_dir = debugfs_create_dir("fortytwo", NULL);
+
+    if (!fortytwo_dir) {
+        pr_err("Failed to create debugfs directory\n");
+        return -ENOMEM;
+    }
+
+    debugfs_create_file("id", 0666, fortytwo_dir, NULL, &id_fops);
+    debugfs_create_file("jiffies", 0444, fortytwo_dir, NULL, &jiffies_fops);
+    debugfs_create_file("foo", 0644, fortytwo_dir, NULL, &foo_fops);
+
+    pr_info("Module loaded\n");
+    return 0;
+}
+
+static void __exit ex06_exit(void) {
+    debugfs_remove_recursive(fortytwo_dir);
+    pr_info("Module unloaded\n");
+}
+
+module_init(ex06_init);
+module_exit(ex06_exit);
---
+++ ex07/test.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+
+# ###test for debug fs
+# ls /sys/kernel/debug/
+# ### in case of fail 
+# mount -t debugfs none /sys/kernel/debug/
+# ###check if enabled in kernel
+# cat /boot/config-6.4.12 | grep CONFIG_DEBUG_FS
+
+### test file creation and permissions
+echo
+echo "Test General:"
+ls -l /sys/kernel/debug/ | grep fortytwo
+ls -l /sys/kernel/debug/fortytwo
+dmesg -T | tail -1
+
+### test id
+echo
+echo "Test id:"
+echo
+cat /sys/kernel/debug/fortytwo/id
+echo 
+echo 'Error expected:'
+echo 123 > /sys/kernel/debug/fortytwo/id
+echo 'Error expected:'
+echo "123nser" > /sys/kernel/debug/fortytwo/id
+echo $?
+echo ogenser > /sys/kernel/debug/fortytwo/id
+echo $?
+dmesg -T | tail -1
+
+### test jiffies
+echo
+echo "Test Jiffies:"
+echo
+echo "My Jiffies:"
+cat /sys/kernel/debug/fortytwo/jiffies
+### check real jiffies
+echo 'Real Jiffies:'
+grep -E "^cpu|^jiff" /proc/timer_list
+echo 
+echo 'Error expected:'
+echo 123 > /sys/kernel/debug/fortytwo/jiffies
+
+### test foo
+echo
+echo "Test Foo:"
+echo
+whoami
+
+echo 123 > /sys/kernel/debug/fortytwo/foo
+cat /sys/kernel/debug/fortytwo/foo
